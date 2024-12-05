3edc12fef903a5470bd382cd8d27bdcd63b1dbcc
Emma Safar <safaremma6@gmail.com>
2024-12-05 11:18:32 +0100

    Poruka

---
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h1>Moja vjezba danas</h1>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
